20471eb5dc7f8e3c71618813f0bd3fd31a8e02ad
Moon <moonkarmakar2609@gmail.com>
2026-04-25 23:38:18 +0530

    my C Codes

---
+++ angletriangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter three angle:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b+c==180)
+    printf("it forms a triangle:");
+    else
+    printf("it not forms a triangle:");
+    return 0;
+}
+
+
+
+
+
