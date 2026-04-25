57da8a3c4cf15bfddbc8be7e75e3a771b01a00f5
Moon <moonkarmakar2609@gmail.com>
2026-04-25 23:36:33 +0530

    my C Codes

---
+++ ageprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+    float height;
+    printf("enter your age \n  ");
+
+    scanf("%d",&age);
+    printf("enter your height \n");
+    scanf("%f",&height);
+    printf("your age is %d and height is %d",age,height);
+    return 0;
+}
