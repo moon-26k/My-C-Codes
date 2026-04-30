6980bb590d204d0b09c2d9ec6b76d5bc32a39c81
Moon <moonkarmakar2609@gmail.com>
2026-04-30 20:17:57 +0530

    My C Codes

---
+++ factor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    i=1;
+    while(i<=n)
+    {
+       if(n%i==0)
+       printf("%d\n",i);
+       i++;
+    }
+    return 0;
+}
