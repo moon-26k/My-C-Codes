ae824a10452a0f73a65e63670e04fbd6c6d30efb
Moon <moonkarmakar2609@gmail.com>
2026-05-01 22:56:22 +0530

    My C Codes

---
+++ findsumofgiveninarray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void trgetsubarray_sum(int arr[], int n, int k , int target){
+     int curr_sum=0;
+for (int i=0;i<k;i++){
+    curr_sum+=arr[i];
+}
+if(curr_sum==target)
+{
+    
+}
+}   
